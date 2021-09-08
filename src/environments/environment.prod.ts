b65e66ad2d0af08cd3f4172282415ac9b41ffd28
winlex <alexandergurin1996@gmail.com>
2021-09-08 19:45:19 +0300

    фикс переменных окружения для прода

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,18 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'http://todo-list-my.herokuapp.com/'
+  APIEndpoint: 'http://todo-list-my.herokuapp.com/',
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyBZh-jY7POclArqZORXWLN1i33TQXKJ5HQ",
+    authDomain: "file-manager-2ee82.firebaseapp.com",
+    databaseURL: "https://file-manager-2ee82-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "file-manager-2ee82",
+    storageBucket: "file-manager-2ee82.appspot.com",
+    messagingSenderId: "768510299451",
+    appId: "1:768510299451:web:63a89715ffaf79c19fbb1e"
+  }
 };
